dbfc1e25f474cec58a5814496e8ad408318ba8b0
AymardKonan <kouadio-aymard.konan@onma.io>
2025-01-06 15:22:25 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,60 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//Route pour le dash
+Route::get('/dashboard', function () {
+
+    return view('dashboard');
+
+})->name('dashboard');
+
+// Route pour les employes
+Route::get('/employes', function () {
+
+    return view('employes.listeEmployes');
+
+})->name('employes');
+
+//Route cotisation
+Route::get('/cotisation', function () {
+
+    return view('cotisation.tabledecotisation');
+
+})->name('cotisation');
+
+
+//Route pour les entreprises
+Route::get('/entreprise', function () {
+
+    return view('entreprise.listeEntreprise');
+
+})->name('entreprise');
+
+//Route pour les emplois&departement
+Route::get('/emplois&departement', function () {
+
+    return view('emploi_departe.listeEmploiDprt');
+
+})->name('emplois&departement');
+
+//Route pour les rapports d'emplois & département
+Route::get('/Rapportemplois&departement', function () {
+
+    return view('emploi_departe.rapportEmploiDprt');
+
+})->name('Rapportemplois&departement');
+
+//Route pour les localités
+Route::get('/Localites', function () {
+
+    return view('localites.localites');
+
+})->name('Localites');
+
+
+// Route rapport globale
+Route::get('/Rapportglobale', function () {
+
+    return view('rapportStat.rapportglobale');
+
+})->name('rapportglobale');
